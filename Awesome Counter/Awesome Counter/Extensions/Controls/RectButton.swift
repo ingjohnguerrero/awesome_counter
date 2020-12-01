90ff34033cee1e253a571e3f5cb7f4f99415db6a
John Guerrero <ingjohnguerrero@gmail.com>
2020-12-01 01:56:37 -0500

    feat (WelcomeView): Add assets, fonts and layout

---
+++ Awesome Counter/Awesome Counter/Extensions/Controls/RectButton.swift
@@ -0,0 +1,20 @@
+//
+//  RectButton.swift
+//  Awesome Counter
+//
+//  Created by John Guerrero on 1/12/20.
+//
+
+import UIKit
+
+class RectButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,3 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Add Changelog
 - Add Travis ci integration
 - Add Sonar Cloud to the project
+- Add splash view to project
+- Add Color assets
+- Add icon assets
+- Add fonts
