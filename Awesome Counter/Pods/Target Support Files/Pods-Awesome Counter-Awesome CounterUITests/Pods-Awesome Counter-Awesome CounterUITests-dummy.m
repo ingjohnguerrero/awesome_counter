184de5ff74460f6d8ec9ede421214b529315a32a
John Guerrero <ingjohnguerrero@gmail.com>
2020-12-11 16:49:54 -0500

    feat (gitignore): Remove Pods

--- Awesome Counter/Pods/Target Support Files/Pods-Awesome Counter-Awesome CounterUITests/Pods-Awesome Counter-Awesome CounterUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Awesome_Counter_Awesome_CounterUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Awesome_Counter_Awesome_CounterUITests
-@end
